71f765009d37eda35ef14a6a338618225cde0336
Yuriy Bondarchuk <bondarchook@gmail.com>
2016-03-16 13:40:08 +0100

    Add New-Publication and Set-Publication commands

--- CoreService/Items.psm1
+++ CoreService/Items.psm1
@@ -156,6 +156,329 @@ Function Get-Item
 	}
 }
 
+function New-Publication
+{
+    <#
+    .Synopsis
+    Adds a new publication to Tridion Content Manager.
+
+    .Description
+    Adds a new publication to Tridion Content Manager with the given title, description, parrents and other parameters.  
+
+    .Notes
+    Example of properties available: Title, PublicationKey, PublicationPath, PublicationUrl, MultimediaUrl, ParrentPublications, etc.
+    
+    For a full list, consult the Content Manager Core Service API Reference Guide documentation 
+    (Tridion.ContentManager.Data.CommunicationManagement.PublicationData object)
+
+    .Inputs
+    [string] title: the title of publication.
+    [string] publicationKey: the publication Publication key.
+    [string] publicationPath: the publication Publication path.
+    [string] publicationURL: the publication Publication URL.
+    [string] multimediaPath: the publication Multimedia path.
+    [string] multimediaUrl: the publication Multimedia URL.
+    [string] businessProcessType: the publications Business Process Type.
+    [string[]] ParrentPublications: the parrents of publication.
+    
+    .Outputs
+    Returns an object of type [Tridion.ContentManager.CoreService.Client.PublicationData], representing the newly created publication.
+
+    .Link
+    Get the latest version of this script from the following URL:
+    https://github.com/pkjaer/tridion-powershell-modules
+
+    .Example
+    New-Publication -Title "TestPub"
+    Adds publication with title "TestPub" and publication key "TestPub" to the Content Manager.
+	
+	.Example
+    New-Publication -Title "TestPub" -ParrentPublications "Pub1", "tcm:0-209-1"
+    Adds "TestPub" publication to the Content Manager with 2 parent publications. Publication with title "Pub1" and publication with id "tcm:0-209-1".
+
+	.Example
+    New-Publication -Title "TestPub" -PublicationKey "SomeKey"
+    Adds publication with title "TestPub" and key "SomeKey" to the Content Manager.
+    
+    #>
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
+    Param(
+			# The Title of publication.
+            [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+			[ValidateNotNullOrEmpty()]
+            [string]$Title,
+			
+            # The PublicationKey of publication.
+            [Parameter()]
+            [string]$PublicationKey,
+			
+			# The PublicationPath of publication.
+            [Parameter()]
+            [string]$PublicationPath,
+			
+            # The PublicationURL of publication.
+            [Parameter()]
+            [string]$PublicationURL,
+            
+			# The MultimediaPath of publication.
+            [Parameter()]
+            [string]$MultimediaPath,
+            
+			# The MultimediaUrl of publication.
+            [Parameter()]
+            [string]$MultimediaUrl,
+            
+			# The Business Process Type of publication.
+            [Parameter()]
+            [string]$BusinessProcessType,
+
+			# The parrents of publication.
+            [Parameter()]
+            [string[]]$ParrentPublications
+    )
+	
+	Begin
+	{
+        $client = Get-CoreServiceClient -Verbose:($PSBoundParameters['Verbose'] -eq $true);
+	}
+
+    Process
+    {
+        if ($client -ne $null)
+        {
+			$readOptions = New-Object Tridion.ContentManager.CoreService.Client.ReadOptions;
+			$readOptions.LoadFlags = [Tridion.ContentManager.CoreService.Client.LoadFlags]::None;
+			
+			if ($client.GetDefaultData.OverloadDefinitions[0].IndexOf('ReadOptions readOptions') -gt 0)
+			{
+				$pub = $client.GetDefaultData("Publication", $null, $readOptions);
+			}
+			else
+			{
+				$pub = $client.GetDefaultData("Publication", $null);
+			}
+			
+			SetPublicationProperties $pub  $Title  $PublicationKey  $PublicationPath  $PublicationURL  $MultimediaPath  $MultimediaUrl  $BusinessProcessType  $ParrentPublications
+
+			if ($PSCmdLet.ShouldProcess("Publication { Title: '$($pub.Title)' }", "Create")) 
+			{
+				$client.Create($pub, $readOptions);
+				Write-Verbose ("Publication '{0}' has been created." -f $pub.Title);
+			}
+        }
+    }
+	
+	End
+	{
+		Close-CoreServiceClient $client;
+	}	
+}
+
+function Set-Publication
+{
+    <#
+    .Synopsis
+    Update publication.
+
+    .Description
+    Update publication with the given title, description, parrents and other parameters.  
+
+    .Notes
+    Example of properties available: Title, PublicationKey, PublicationPath, PublicationUrl, MultimediaUrl, ParrentPublications, etc.
+    
+    For a full list, consult the Content Manager Core Service API Reference Guide documentation 
+    (Tridion.ContentManager.Data.CommunicationManagement.PublicationData object)
+
+    .Inputs
+    [string] title: the title of publication.
+    [string] publicationKey: the publication Publication key.
+    [string] publicationPath: the publication Publication path.
+    [string] publicationURL: the publication Publication URL.
+    [string] multimediaPath: the publication Multimedia path.
+    [string] multimediaUrl: the publication Multimedia URL.
+    [string] businessProcessType: the publications Business Process Type.
+    [string[]] ParrentPublications: the parrents of publication. Note that by setting ParrentPublications you override existing paretns
+    
+    .Outputs
+    Returns an object of type [Tridion.ContentManager.CoreService.Client.PublicationData], representing the newly created publication.
+
+    .Link
+    Get the latest version of this script from the following URL:
+    https://github.com/pkjaer/tridion-powershell-modules
+
+    .Example
+    Set-Publication -Id "tcm:0-1-1" -Title "TestPub"
+    Set title of publication to "TestPub".
+	
+	.Example
+    Set-Publication -Title "TestPub" -ParrentPublications "Pub1", "tcm:0-209-1"
+    Set title of publication to "TestPub" and override parents. Publication with title "Pub1" and publication with id "tcm:0-209-1".
+
+	.Example
+    Set-Publication -Title "TestPub" -PublicationKey "SomeKey"
+    Set title of publication to "TestPub" and publication key "SomeKey".
+    
+    #>
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
+    Param(
+			# The TcmUri of publication.
+            [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+			[ValidateNotNullOrEmpty()]
+            [string]$Id,
+
+			# The Title of publication.
+            [Parameter()]
+            [string]$Title,
+			
+            # The PublicationKey of publication.
+            [Parameter()]
+            [string]$PublicationKey,
+			
+			# The PublicationPath of publication.
+            [Parameter()]
+            [string]$PublicationPath,
+			
+            # The PublicationURL of publication.
+            [Parameter()]
+            [string]$PublicationURL,
+            
+			# The MultimediaPath of publication.
+            [Parameter()]
+            [string]$MultimediaPath,
+            
+			# The MultimediaUrl of publication.
+            [Parameter()]
+            [string]$MultimediaUrl,
+            
+			# The Business Process Type of publication.
+            [Parameter()]
+            [string]$BusinessProcessType,
+
+			# The parrents of publication.
+            [Parameter()]
+            [string[]]$ParrentPublications
+    )
+	
+	Begin
+	{
+        $client = Get-CoreServiceClient -Verbose:($PSBoundParameters['Verbose'] -eq $true);
+	}
+
+    Process
+    {
+        if ($client -ne $null)
+        {
+			$pub = Get-TridionItem -Id $Id
+
+			SetPublicationProperties $pub  $Title  $PublicationKey  $PublicationPath  $PublicationURL  $MultimediaPath  $MultimediaUrl  $BusinessProcessType  $ParrentPublications
+
+			if ($PSCmdLet.ShouldProcess("Publication { Title: '$($pub.Title)' }", "Create")) 
+			{
+				$client.Update($pub, $readOptions);
+				Write-Verbose ("Publication '{0}' has been created." -f $pub.Title);
+			}
+        }
+    }
+	
+	End
+	{
+		Close-CoreServiceClient $client;
+	}	
+}
+
+function SetPublicationProperties ($pub, [string]$Title, [string]$PublicationKey, [string]$PublicationPath, [string]$PublicationURL, [string]$MultimediaPath, [string]$MultimediaUrl, [string]$BusinessProcessType, [string[]]$ParrentPublications)
+{
+	$tridionPublications = $null;
+	$publicationsLoaded = $false;
+
+	if($Title)
+	{
+		$pub.Title = $Title
+	}
+
+	if($PublicationKey)
+	{
+		$pub.Key = $PublicationKey
+	}
+	elseif ($Title)
+	{
+		$pub.Key = $Title
+	}
+
+	if($PublicationPath)
+	{
+	    $pub.PublicationPath = $PublicationPath
+	}
+
+	if($PublicationURL)
+	{
+		$pub.PublicationUrl = $PublicationURL
+	}
+
+	if($MultimediaPath)
+	{
+		$pub.MultimediaPath = $MultimediaPath
+	}
+
+	if($MultimediaUrl)
+	{
+		$pub.MultimediaUrl = $MultimediaUrl
+	}
+
+	if ($ParrentPublications)
+	{
+		#$pub.Parents = $()
+
+		foreach($pubUri in $ParrentPublications)
+		{
+			if ($pubUri)
+			{
+				if (-not $pubUri.StartsWith('tcm:'))
+				{
+					# It's not a URI, it's a name. Look up the group URI by its title.
+					if (-not $publicationsLoaded)
+					{
+						$tridionPublications = Get-Publications
+						$publicationsLoaded = $true;
+					}
+							
+					$parentPub = $tridionPublications | ?{$_.Title -eq $pubUri} | Select -First 1
+					if (-not $parentPub) 
+					{
+						Write-Error "Could not find a publication named $pubUri."
+						continue
+					}
+							
+					$pubUri = $parentPub.id
+				}
+
+				$pubLink = New-Object Tridion.ContentManager.CoreService.Client.LinkToRepositoryData;
+				$pubLink.IdRef = $pubUri;
+						
+				$pub.Parents += $pubLink;
+			}
+
+			$pub.Parents
+		}
+	}
+
+	if($BusinessProcessType)
+	{
+		$bpt = Get-TridionItem -Id $BusinessProcessType -ErrorAction SilentlyContinue
+		if ($bpt)
+		{
+			$bptLink = New-Object Tridion.ContentManager.CoreService.Client.LinkToBusinessProcessTypeData;
+			$bptLink.IdRef = $bpt.Id;
+			$pub.BusinessProcessType = $bptLink;
+		}
+		else
+		{
+			Write-Error "Could not find a Business Process Type with id $BusinessProcessType."
+		}
+	}
+}
+
+
 <#
 **************************************************
 * Export statements
@@ -163,3 +486,5 @@ Function Get-Item
 #>
 Export-ModuleMember Get-Item
 Export-ModuleMember Get-Publications
+Export-ModuleMember New-Publication
+Export-ModuleMember Set-Publication
